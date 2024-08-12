95c0d1ac5c46819072cb3a342c7e391d14e662d8
Bogdan <bogdan@Bogdans-Laptop.local>
2024-08-13 01:28:35 +0300

    0.3

--- App.java
+++ App.java
@@ -5,29 +5,37 @@ import java.util.Scanner;
 
 public class App {
 
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         while (true)
         {
-            System.out.println("~~~~ MAIB Terminal App ~~~~");
+            clearTerminal();
+            System.out.println(GREEN + "~~~~ Welcome to MAIB ~~~~" + RESET);
 
             System.out.println("\n1. Register");
             System.out.println("2. Log in");
             System.out.println("3. Exit");
 
-            System.out.print("Enter your choice: ");
+            System.out.print(YELLOW + "\nEnter your choice: " + RESET);
             String choice = sc.nextLine();
 
             switch (choice)
             {
                 case "1":
-//                    RegisterUser();
+                    RegisterUser();
+                    break;
                 default:
                     clearTerminal();
-                    System.out.println("Invalid choice!");
-
+                    System.out.println(RED + "Invalid choice!" + RESET);
+                    delayTime();
             }
         }
 
@@ -35,7 +43,28 @@ public class App {
 
     }
 
+    // Log In Menu Methods
+    public static void RegisterUser()
+    {
+        clearTerminal();
+        System.out.println("~~~~ MAIB ~~~~");
+        System.out.println("\n-> Register yourself");
+
+        System.out.print("\nEnter Your First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter Your Second: ");
+        String secondName = sc.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = sc.nextLine();
+
+
+    }
+
+
     // UI voids
+
     public static void clearTerminal()
     {
         System.out.println("\033[H\033[2J");
